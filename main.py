611bc2655e4931a257a34de5c2e807137a936deb
Lidor Eliyahu Shelef <ivanlidor98@gmail.com>
2022-03-01 20:12:03 +0200

    Added \ Updated:  - Default option to pass no arguments and have the methods use the default images folder  - Default main file that will activate the default folder on each exercise of the checker choosing  - Change Readme.doc to README.doc  - Added tables of content, code instructions (per situation) and ex 2 examples to the README.doc

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from projectIM2022_q1 import apply_ex1_detection
 from projectIM2022_q2 import apply_ex2_detection
 
-print("Insert q or Q to quit mate!")
+"""
+    Authors: Yuval Avni, Lidor Eliyahu Shelef
+"""
+
+quits = ["q", "Q", "x", "X"]
+print("Insert one of the following in order to quit mate!")
+print(quits)
 while True:
     ex_number = input("Please select which exercise you wish to activate (insert 1 or 2 accordingly)\n>>> ")
 
@@ -9,6 +15,6 @@ while True:
         apply_ex1_detection()
     elif ex_number == str(2):
         apply_ex2_detection()
-    elif ex_number == "q" or ex_number == "Q":
+    elif ex_number in quits:
         print("Goodbye, we hope you had fun using our program \U0001f604")
         break
